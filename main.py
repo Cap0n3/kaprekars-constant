c3f542f881f1b6d402a197e4627b57d871b5ab5f
cap0n3 <cap0n3@protonmail.com>
2024-04-28 17:59:29 +0200

    Testing 3 numbers routine

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from kaprekar.utils import is_valid_number
 from kaprekar.kaprekar_routine import kaprekar_routine
 
 if __name__ == "__main__":
-    while True:
-        input_nb = int(input("Please, enter a 4-digit number: "))
-        if is_valid_number(input_nb):
-            break
-        print("Invalid input. Ensure you enter a 4-digit number with at least two different digits.")
+   
+    input_nb = int(input("Please, enter a 4-digit number: "))
+        # if is_valid_number(input_nb):
+        #     break
+        # print("Invalid input. Ensure you enter a 4-digit number with at least two different digits.")
 
     kaprekar_routine(input_nb)
